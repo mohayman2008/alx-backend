e1c3ba2bb3ab4a1d2dfb147aeefc8266d17b92d9
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-08 18:34:10 +0000

    0x01. Caching - Tasks 1 & 2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+'''This module contains the declaration of class "LIFOCache"'''
+from collections import OrderedDict
+from typing import Any, Hashable, Optional
+
+from base_caching import BaseCaching
+
+
+class LIFOCache (BaseCaching):
+    '''BasicCache: caching system using LIFO replacement algorithm'''
+
+    def __init__(self):
+        '''Constructor'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key: Optional[Hashable], item: Optional[Any]):
+        '''Add an item in the cache'''
+        if key is None or item is None:
+            return
+
+        keys = self.cache_data.keys()
+        if key in keys:
+            self.cache_data.move_to_end(key)
+        elif len(keys) >= self.MAX_ITEMS:
+            popped_key, pooped_val = self.cache_data.popitem()
+            print(f"DISCARD: {popped_key}")
+
+        self.cache_data[key] = item
+
+    def get(self, key: Optional[Hashable]) -> Optional[Any]:
+        '''Get an item by key'''
+        if key is None:
+            return None
+        return self.cache_data.get(key, None)
