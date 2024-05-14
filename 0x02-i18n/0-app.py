b22b3234c70bf1d43c23cf29fe02bdf77fef3770
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-14 10:44:13 +0000

    0x02. i18n - Mandatory tasks 0 & 1

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Simple Flask app'''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    '''Configuration class for the Flask <app>'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/", strict_slashes=False)
+def index() -> str:
+    '''The root route'''
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
