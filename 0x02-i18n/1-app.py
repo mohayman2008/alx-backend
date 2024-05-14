a3bc3bbe0ad0d406c5894379ab40822848edea76
Mohamed Ayman <mohayman2007@gmail.com>
2024-05-14 10:52:47 +0000

    0x02. i18n - Mandatory tasks 0 & 1 - Fixing files names

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 '''Simple Flask app'''
 from flask import Flask, render_template
+from flask_babel import Babel  # type: ignore
+
+
+class Config():
+    '''Configuration class for the Flask <app>'''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route("/", strict_slashes=False)
